803521ee226fe06131c4f49b8fd7851bde15e33b
arangaraBrown <157825306+arangaraBrown@users.noreply.github.com>
2025-10-17 21:38:40 -0400

    Remove API credentials

--- tools/dashboard/Qualtrics_API_requests.py
+++ tools/dashboard/Qualtrics_API_requests.py
@@ -4,8 +4,8 @@ import pandas as pd
 import datetime as dt
 
 def get_qualtrics_data():
-    Credentials().qualtrics_api_credentials(token='9ISA2H5BZcTcyKLOVbGNWCWiipf4wz9fLzYGMZ6X', data_center='iad1')
-    df = Responses().get_survey_responses(survey='SV_aavJvw1DJO7ogB0',verify=None,useLabels=True)
+    Credentials().qualtrics_api_credentials(token='token', data_center='iad1')
+    df = Responses().get_survey_responses(survey='survey_id',verify=None,useLabels=True)
     df = df[df['Finished'] == 'True']
     df['RecordedDate'] = pd.to_datetime(df['RecordedDate'])
     df = df[df['RecordedDate'] >= '2024-08-01']
